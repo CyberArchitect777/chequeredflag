81460005b6afa3c53d3161ebe170afdfca51518a
Barrie Millar <169656116+CyberArchitect777@users.noreply.github.com>
2005-02-23 22:19:18 +0000

    First project commit inside the chequeredflag directory (I hope)

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * Main.java
+ *
+ * Created on 22 February 2005, 22:23
+ */
+
+package Chequeredflag;
+
+/**
+ *
+ * @author barrie
+ */
+public class Main 
+{
+    
+    private MainGUI mainScreen;
+    
+    /** Creates a new instance of Main */
+    public Main() 
+    {
+           mainScreen = new MainGUI();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Main CheqFlag = new Main();
+    }
+    
+}
