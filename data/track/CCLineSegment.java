ca911c86893a919125e858f8f3672c986606aee2
ksix <ksix>
2005-10-17 21:21:02 +0000

    get/setParam Bugfix for index value. New method getShift for abstract parameter access. Added a number of calculated values with access methods.

--- data/track/CCLineSegment.java
+++ data/track/CCLineSegment.java
@@ -29,13 +29,13 @@ public class CCLineSegment extends CFDataObject {
         try
         {
             // All CCLine segments have at least two parameters
-            m_nParam[ 0 ] = fis.read() + fis.read() * 256;
-            m_nParam[ 1 ] = fis.read() + fis.read() * 256;
+            m_nParam[ 0 ] = loadInt( fis );
+            m_nParam[ 1 ] = loadInt( fis );
             // Rest depends on CCLine segment type
             if ( m_nType != 0 )
             {
                 // Should be a type 0x80 (first segment) or 0x40 (32bit radius) segment
-                m_nParam[ 2 ] = fis.read() + fis.read() * 256;
+                m_nParam[ 2 ] = loadInt( fis );
             }
         }
         catch( IOException ioe )
@@ -54,10 +54,21 @@ public class CCLineSegment extends CFDataObject {
     public void setType( int nType )
     { m_nType = nType; }
 
+    public int getShift()
+    {
+        int nShift;
+        if ( ( m_nType & 0x80 ) > 0 )
+            nShift = m_nParam[1];
+        else
+            nShift = m_nParam[0];
+        return nShift;
+    }
+
+    // Params are stored and retrieved by 0-based index
     public int getParam( int nIndex )
-    { return m_nParam[ nIndex + 1 ]; }
+    { return m_nParam[ nIndex ]; }
     public void setParam( int nIndex, int nValue )
-    { m_nParam[ nIndex + 1 ] = nValue; }
+    { m_nParam[ nIndex ] = nValue; }
 
     // save object to disk
     public int save(FileOutputStream fos) throws IOException
@@ -81,7 +92,86 @@ public class CCLineSegment extends CFDataObject {
         return nBytesWritten;
     }
 
+    public double getRadius() {
+        return m_dRadius;
+    }
+
+    public void setRadius(double dRadius) {
+        m_dRadius = dRadius;
+    }
+
+    public double getPosXStart() {
+        return m_dPosXStart;
+    }
+
+    public void setPosXStart(double dPosXStart) {
+        m_dPosXStart = dPosXStart;
+    }
+
+    public double getPosYStart() {
+        return m_dPosYStart;
+    }
+
+    public void setPosYStart(double dPosYStart) {
+        m_dPosYStart = dPosYStart;
+    }
+
+    public double getPosXEnd() {
+        return m_dPosXEnd;
+    }
+
+    public void setPosXEnd(double dPosXEnd) {
+        m_dPosXEnd = dPosXEnd;
+    }
+
+    public double getPosYEnd() {
+        return m_dPosYEnd;
+    }
+
+    public void setPosYEnd(double dPosYEnd) {
+        m_dPosYEnd = dPosYEnd;
+    }
+
+    public double getPosXCenter() {
+        return m_dPosXCenter;
+    }
+
+    public void setPosXCenter(double dPosXCenter) {
+        m_dPosXCenter = dPosXCenter;
+    }
+
+    public double getPosYCenter() {
+        return m_dPosYCenter;
+    }
+
+    public void setPosYCenter(double dPosYCenter) {
+        m_dPosYCenter = dPosYCenter;
+    }
+
+    public double getAngleStart() {
+        return m_dAngleStart;
+    }
+
+    public void setAngleStart(double dAngleStart ) {
+        m_dAngleStart = dAngleStart;
+    }
+
+    public double getAngleEnd() {
+        return m_dAngleEnd;
+    }
+
+    public void setAngleEnd(double dAngleEnd ) {
+        m_dAngleEnd = dAngleEnd;
+    }
+
     // instance data members
     protected int m_nType, m_nTlu;
     protected int m_nParam[];
+
+    // calculated values
+    private double m_dRadius;
+    private double m_dPosXStart, m_dPosYStart;
+    private double m_dPosXEnd, m_dPosYEnd;
+    private double m_dPosXCenter, m_dPosYCenter;
+    private double m_dAngleStart, m_dAngleEnd;
 }
