e5e965b19a6ef2b98cf88121c0ebe21493df1509
Barrie Millar <169656116+CyberArchitect777@users.noreply.github.com>
2006-12-20 00:49:52 +0000

    Added copyright notices to all files

--- data/track/Seg.java
+++ data/track/Seg.java
@@ -1,10 +1,29 @@
+/*
+ * Chequered Flag: An editor for Formula One Grand Prix/World Circuit
+ * Copyright (C) 2005-2006  The Chequered Flag Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 package chequeredflag.data.track;
 
 /*
  * User: Rene
  * Date: 18-okt-2005
  * Time: 22:11:29
- * $Id: Seg.java,v 1.1 2006/09/18 22:12:32 ksix Exp $
+ * $Id: Seg.java,v 1.2 2006/12/20 00:49:52 b-za Exp $
  */
 
 public class Seg {
--- gui/HeaderTableModel.java
+++ gui/HeaderTableModel.java
@@ -1,4 +1,23 @@
 /*
+ * Chequered Flag: An editor for Formula One Grand Prix/World Circuit
+ * Copyright (C) 2005-2006  The Chequered Flag Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/*
  * HeaderTableModel.java
  *
  * Created on November 24, 2005, 6:50 PM
