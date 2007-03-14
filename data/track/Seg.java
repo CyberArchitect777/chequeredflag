b66ef90f4c4ec87442e2ab79414e3e2aa3f78bff
ksix <ksix>
2007-03-14 21:44:48 +0000

    Access methods added. Type of coordinate values changed.

--- data/track/Seg.java
+++ data/track/Seg.java
@@ -23,17 +23,47 @@ package chequeredflag.data.track;
  * User: Rene
  * Date: 18-okt-2005
  * Time: 22:11:29
- * $Id: Seg.java,v 1.3 2007/01/10 15:59:10 b-za Exp $
+ * $Id: Seg.java,v 1.4 2007/03/14 21:44:48 ksix Exp $
  */
 
 public class Seg {
 	short wAngleZ;
 	short wAngleXChase;
-	short wPosX;
-	short wPosZ;
-	short wPosY;
+	int nPosX;
+	int nPosZ;
+	int nPosY;
 	int wCCLineRAngle;
 	int wCCLine;
 	int wAngleZChangeMulHalfPI;
-	short bPosFine;	// 3 bits for x in low nibble, 3 bits for y in high nibble
+	short bPosFine;	// 3 bits for x in low nibble, 3 bits for y in high nibble NOT USED HERE!
+
+        public int getPosX() { return nPosX; };    // for hiding details of x/y pos storage (bPosFine)
+        public int getPosY() { return nPosY; };
+        public int getPosZ() { return nPosZ; };
+
+        /**
+            Unlike F1GP internal storage, X and Y position are directly stored as 32 bit int.
+            bPosFine is not used as long as it is not definitely needed (KS 13.03.07)
+        */
+        public void setPos( int nNewPosX, int nNewPosY, int nNewPosZ )
+        {
+            nPosX = nNewPosX;
+            nPosY = nNewPosY;
+            nPosZ = nNewPosZ;
+        };
+
+        public int getTrackWidth()
+        { return 512; } // @@@ not set yet
+
+        public int getAngleZ()
+        { return wAngleZ; };
+
+        public int getAngleXChase()
+        { return wAngleXChase; }
+
+        public int getCCLine()
+        { return wCCLine; };
+
+        public int getCCLineRAngle()
+        { return wCCLineRAngle; }
 }
